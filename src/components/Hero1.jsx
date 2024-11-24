acba6a399dd73c55361ccafad297c4fef0f024c0
Arko <arkotesla@gmail.com>
2024-11-24 11:12:05 +0600

    running123

---
+++ src/components/Hero1.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Hero1 = () => {
+  return (
+    <>
+      <div
+        className="text-white p-6 rounded-md bg-cover bg-center relative h-screen flex items-center justify-center"
+        style={{ backgroundImage: "url('/images/European/finland.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+        <div className="relative text-center text-white">
+          <h1 className="text-5xl font-bold font-against mb-4 animate-pulse md:text-xl">
+            Welcome to{" "}
+            <span className="animate-inflate-deflate">NebulaQuest</span>
+          </h1>
+          <p className="md:text-lg mb-6 font-against animate-pulse">
+            Explore the world’s most beautiful destinations with us.
+          </p>
+          <Link
+            to="/Home"
+            className="px-6 py-3 rounded-lg shadow-lg font-against text-white hover:scale-105 transform animate-pulse
+              transition-transform duration-300 ease-in-out bg-white/5 backdrop-blur-md border border-white"
+          >
+            Start Your Journey
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero1;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Hero1 from "../components/Hero1";
 
 
 function Home() {
   return (
     <>
-     
+     <Hero1/>
     </>
   );
 }
