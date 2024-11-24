7fec727b85a2e636bfb766e1335f6c9e858cc45f
Arko <arkotesla@gmail.com>
2024-11-24 11:33:30 +0600

    fear

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Hero1 from "../components/Hero1";
+import Showcase from "../components/Showcase";
+import Footer from "../components/Footer";
 
 
 function Home() {
   return (
     <>
       <Hero1 />
-      <Showcase />
-      <Footer />
+     <Showcase/>
+     <Footer/>
+     
     </>
   );
 }
